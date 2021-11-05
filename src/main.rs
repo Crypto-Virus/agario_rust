96f30ea6778810e8df959ed94e844587b910b58f
Virus <zxcv.fghj.uiop@gmail.com>
2021-11-05 00:23:25 -0400

    Add development environment variables

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,16 @@ use tokio::net::TcpListener;
 use agario_rust::server;
 use agario_rust::config;
 
-
+const DEV: bool = true;
 
 #[tokio::main]
 async fn main() -> agario_rust::Result<()> {
-    dotenv::dotenv().ok();
+
+    if DEV {
+        dotenv::from_filename("dev.env").ok();
+    } else {
+        dotenv::dotenv().ok();
+    }
 
     let config = config::load_config();
     let listener = TcpListener::bind(&format!("0.0.0.0:{}", config.ws_port)).await?;
